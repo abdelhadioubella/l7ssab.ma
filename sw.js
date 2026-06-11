8ebbb420e21441f115f63c3fc623552b13862758
abdelhadioubella <167465731+abdelhadioubella@users.noreply.github.com>
2026-06-11 19:27:37 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,18 +1,9 @@
-<<<<<<< HEAD
-var CACHE='l7ssab-v5';
+var CACHE='l7ssab-v6';
 var ASSETS=[
 './','./index.html','./manifest.json',
 './css/styles.css',
 './js/config.js','./js/login.js','./js/inventory.js','./js/products.js','./js/adjustments.js','./js/recap.js','./js/profile.js','./js/database.js','./js/statistics.js','./js/users.js','./js/adminprojects.js',
 './pages/inventory.html','./pages/products.html','./pages/adjustments.html','./pages/recap.html','./pages/profile.html','./pages/database.html','./pages/statistics.html','./pages/users.html','./pages/projects.html',
-=======
-var CACHE='l7ssab-v4';
-var ASSETS=[
-'./','./index.html','./manifest.json',
-'./css/styles.css',
-'./js/config.js','./js/login.js','./js/inventory.js','./js/products.js','./js/adjustments.js','./js/recap.js','./js/profile.js','./js/database.js','./js/users.js','./js/adminprojects.js',
-'./pages/inventory.html','./pages/products.html','./pages/adjustments.html','./pages/recap.html','./pages/profile.html','./pages/database.html','./pages/users.html','./pages/projects.html',
->>>>>>> b431d67383dde2c022f7a4f776f603a97d6d6512
 './assets/icon-192.png','./assets/icon-512.png'
 ];
 self.addEventListener('install',function(e){self.skipWaiting();e.waitUntil(caches.open(CACHE).then(function(c){return c.addAll(ASSETS).catch(function(){});}));});
